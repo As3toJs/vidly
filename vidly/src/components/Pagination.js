65ad600c9b8410999f9a0101664162a700842ccb
piyush <piyush@edulab.dk>
2018-08-22 07:43:16 +0200

    added pagination. working..

---
+++ vidly/src/components/Pagination.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { range } from 'ramda';
+
+const Pagination = ({ pageLimit, currentPage, total, onSelectePage }) => {
+    const pages = Math.ceil(total / pageLimit);
+    if (pages === 1) return null;
+    const pageRange = range(1, pages);
+    console.log("range =>", pageRange.length);
+
+    return (
+        <nav>
+            <ul className="pagination">
+                {pageRange.map(page => {
+                    return (
+                        <li key={page} className={currentPage === page ? "page-item active" : "page-item"}>
+                            <a
+                                className="page-link"
+                                onClick={() => onSelectePage(page)}>
+                                {page}
+                            </a>
+                        </li>
+                    )
+                })}
+            </ul>
+        </nav>
+    )
+}
+
+export default Pagination;
