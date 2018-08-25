ef9921cfe6f5c11b37ab4f501b03b4d0ae396c8b
piyush <piyush@edulab.dk>
2018-08-25 15:21:51 +0200

    Added filters on the list

--- vidly/src/components/Movies.js
+++ vidly/src/components/Movies.js
@@ -1,11 +1,17 @@
 import React, { Component } from 'react';
 import { getMovies, deleteMovie } from "../services/fakeMovieService";
-import Like from './Like';
+import { genres, getGenres } from "../services/fakeGenreService";
+
 import Pagination from './Pagination';
 import { paginate } from '../utils/paginate';
+import ListGroup from './ListGroup';
+import './Movies.css';
+import MoviesTable from './MoviesTable';
 
 export default class Movies extends Component {
     state = {
+        genres: [{ _id: 0, name: "All Genres" }, ...genres],
+        selectedGenre: "All Genres",
         movies: getMovies(),
         pageLimit: 4,
         currentPage: 1,
@@ -25,63 +31,41 @@ export default class Movies extends Component {
         return '</span>There are no movies in the database</span>';
     }
 
+    handleSelectGenres = (selected) => {
+        this.setState({ selectedGenre: selected.name })
+    }
+
     renderMovieTable = () => {
-        const { movies, currentPage, pageLimit } = this.state;
-        const { length: movieCount } = this.state.movies;
-        const moviesPerPage = paginate(movies, currentPage, pageLimit);
+        const { movies, currentPage, pageLimit, genres, selectedGenre } = this.state;
+        const filteredMovies = selectedGenre !== 'All Genres' ?
+            movies.filter(movie => movie.genre.name === selectedGenre) :
+            movies;
+        const moviesPerPage = paginate(filteredMovies, currentPage, pageLimit);
         return (
-            <div>
-                <span>
-                    Showing {movieCount} movies in the database.
-                </span>
-                <table className="table">
-                    <thead>
-                        <tr>
-                            <th scope="col">Title</th>
-                            <th scope="col">Genre</th>
-                            <th scope="col">Stock</th>
-                            <th scope="col">Rate</th>
-                            <th></th>
-                            <th></th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {moviesPerPage.map(movie => {
-                            return this.renderRow(movie)
-                        }
-                        )}
-                    </tbody>
-                </table>
-                <Pagination
-                    pageLimit={pageLimit}
-                    currentPage={currentPage}
-                    total={movies.length}
-                    onSelectePage={this.onSelectPageHandler}
-                />
+            <div className="vidly">
+                <ListGroup
+                    genres={genres}
+                    selectedGenre={selectedGenre}
+                    selectGenres={this.handleSelectGenres} />
+                <div className="movieList">
+                    <p>Showing {filteredMovies.length} movies in the database.</p>
+                    <MoviesTable
+                        movies={moviesPerPage}
+                        handleDeleteMovie={this.onHandleDeleteMovie}
+                    />
+                    <Pagination
+                        pageLimit={pageLimit}
+                        currentPage={currentPage}
+                        total={filteredMovies.length}
+                        onSelectePage={this.onSelectPageHandler}
+                    />
+                </div>
             </div>
         )
     }
 
     onSelectPageHandler = (page) => {
         this.setState({ currentPage: page })
-    }
-
-    renderRow = (items) => {
-        return (
-            <tr key={items._id}>
-                <td>{items.title}</td>
-                <td>{items.genre.name}</td>
-                <td>{items.numberInStock}</td>
-                <td>{items.dailyRentalRate}</td>
-                <td>{
-                    <Like
-                        items={items}
-                        onLikeClicked={this.likeClicked}
-                    />}
-                </td>
-                <td>{this.renderButton(items._id)}</td>
-            </tr>
-        )
     }
 
     likeClicked = (movie) => {
@@ -91,11 +75,7 @@ export default class Movies extends Component {
         this.setState({ movies });
     }
 
-    renderButton = (id) => {
-        return <button onClick={() => this.handleDeleteMovie(id)} className="btn btn-danger">Delete</button>
-    }
-
-    handleDeleteMovie = (id) => {
+    onHandleDeleteMovie = (id) => {
         deleteMovie(id);
         this.setState({ movies: getMovies() })
     }
